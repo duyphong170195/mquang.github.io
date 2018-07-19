bcb2099ff688fe141a4e251b162a8e019131a072
minhquang <mqfrontendtime@gmail.com>
2018-07-19 17:45:59 +0700

    sua doi lan 1

---
+++ libraries/main.js
@@ -0,0 +1,582 @@
+$('body').append('<i class="fa fa-chevron-circle-up" style="font-size:36px" onclick="topFunction()"></i>')
+$('.container_q').append('<div class="qbo"><input type="checkbox" id="qbo"><label for="qbo">Tăng độ khó (thêm các họ từ liên quan)</label></div>')
+$('.qbo').css({
+      'position':'absolute',
+      'left':$('.entry-main').offset().left,
+      'top':'10px'
+});
+$('#m tbody tr td:nth-child(1)').attr('data-label','Từ / Cụm từ');
+  $('#m tbody tr td:nth-child(2)').attr('data-label','Phiên âm');
+  $('#m tbody tr td:nth-child(3)').attr('data-label','Nghĩa');
+  $('#m tbody tr td:nth-child(4)').attr('data-label','Ví dụ:');
+  var bonus = [];
+$('dfn.ol').each(function(){
+  var a = $(this).html();
+  bonus.push(a);
+})
+
+
+
+var timer;
+
+var ar = [
+  [],
+  [], 
+  []
+]
+
+// array of all the correct answers
+  var QUIZ_ANSWERS = [];
+  $('tbody tr td:nth-child(1)', '#m').each(function(i) {
+      var a = $(this).text();
+      if(a !== ''){
+        QUIZ_ANSWERS.push(a);
+      }
+  });
+  ////console.log(QUIZ_ANSWERS);
+ var sulist = [];
+
+ //Bonus:
+
+  $("dfn.ol").closest('div').each(function(){
+  var oristr = $(this).html();
+  //console.log(oristr);
+  var mySubString = oristr.substring(
+    oristr.lastIndexOf('ol">') + 4, 
+    oristr.indexOf("/dfn>") - 1
+  );
+  //console.log(mySubString);
+  
+  var str2 = oristr.replace(mySubString,"");
+  var str3 = str2.replace('<dfn class="ol"></dfn>',"");
+  var str4 = str3.replace('<li><b>',"");
+  var str5 = str4.replace('</b></li>',"");
+  //console.log(str5);
+  $('#qbo').bind('change', function(){        
+    if($(this).is(':checked') == true){
+      QUIZ_ANSWERS.push(mySubString);
+      sulist.push(str5);
+    }
+    
+    $('.items').find('*').not('h2').remove();
+    sulist = shuffle(sulist);
+     for(var id1 = 0; id1 < sulist.length; id1 ++){
+   $('.items').prepend('<input id="item'+ id1 +'" type="checkbox" onclick="return false" onkeydown="return false"><label for="item' + id1 +'">' + sulist[id1] + '</label>');
+  }
+  clearInterval(timeInterval);
+    reset();
+    $(this).prop("disabled", true);
+  });
+    
+});
+
+//END Bonus
+
+
+$('td:nth-child(3)', '#m').each(function(i) {
+      id1 ++;
+      var b = $(this).html();
+      if(b !== ''){
+        sulist.push(b); 
+      }
+});
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+  sulist = shuffle(sulist);
+  for(var id1 = 0; id1 < sulist.length; id1 ++){
+   $('.items').prepend('<input id="item'+ id1 +'" type="checkbox" onclick="return false" onkeydown="return false"><label for="item' + id1 +'">' + sulist[id1] + '</label>');
+  }
+  var HAPPY_TIME = 1000; // how long to stay happy, in ms
+  var NEUTRAL_TIME = 20000; // how long to stay neutral, in ms
+
+  // timers
+  var toastMoodTimeout;
+  var timeInterval;
+
+  // other global variables
+  var answers;
+  var timeRemaining;
+  var score;
+var rows = QUIZ_ANSWERS.length;   
+
+var QUIZ_TIME_LIMIT;
+
+
+function roundHalf(num) {
+    return Math.round(num*2)/2;
+}
+
+// the time limit, in minutes
+ QUIZ_TIME_LIMIT = roundHalf(rows*10/60);  
+  function startQuiz() {
+    // init some variables
+    initAnswers();
+    timeRemaining = Math.round(QUIZ_TIME_LIMIT * 60);
+    $('#m tr td:nth-child(1)').each(function(){
+      var a = $(this).text();
+      if(rows == 4 && a.length > 13){
+          timeRemaining = 45;
+        }
+    });       
+      if($('#qbo').is(':checked') == true){
+        timeRemaining = Math.round(roundHalf(QUIZ_ANSWERS.length*10/60) * 60);
+      }
+
+    score = 0;
+    
+    // prepare UI
+    $('.time-remaining').text(getTimeString());
+    $('.score').text(score);
+    $('.total').text(QUIZ_ANSWERS.length);
+    $('.start').hide();
+    $('.started').show();
+    $('.input').focus();
+    
+    // start the clock
+          
+    timeInterval = setInterval(reduceTime, 1000);
+    setToastMood('neutral');
+  }
+
+
+
+
+  function initAnswers() {
+    answers = {};
+    QUIZ_ANSWERS.forEach(function(item) {
+      var answer = item.trim().toLowerCase()
+      answers[answer] = false;
+    });
+  }
+
+  function setToastMood(mood, isPermanent) {
+    $('.toast')
+      .removeClass('neutral-toast happy-toast sad-toast')
+      .addClass(mood + '-toast');
+
+    clearTimeout(toastMoodTimeout);
+    if (!isPermanent) {
+      if (mood === 'happy') {
+        // happy toast becomes neutral toast
+        toastMoodTimeout = setTimeout(setToastMood.bind(undefined, 'neutral'), HAPPY_TIME);
+      } else if (mood === 'neutral') {
+        // neutral toast becomes sad toast
+        toastMoodTimeout = setTimeout(setToastMood.bind(undefined, 'sad'), NEUTRAL_TIME);
+      }
+    }
+  }
+
+  function reduceTime() {
+    timeRemaining--;
+    if (timeRemaining === 0) {
+      endQuiz();
+    } else {
+      $('.time-remaining').text(getTimeString());
+    }
+  }
+
+  var voiceSelect = document.getElementById('voice');
+  function loadVoices() { 
+   var voices = speechSynthesis.getVoices();
+
+   voices.forEach(function(voice, i) {
+    var option = document.createElement('option');
+    option.value = voice.name;
+    option.innerHTML = voice.name;
+    voiceSelect.appendChild(option);
+
+   });
+
+   //$('#voice').val('Google UK English Male');
+  }
+  loadVoices();
+  
+  window.speechSynthesis.onvoiceschanged = function(e) {
+    loadVoices();
+  };
+
+
+  function checkInput(event) {
+    var input = event.currentTarget.value.trim().toLowerCase();
+
+    $('tr td:nth-child(1)', '#m').each(function(i) {
+        var a = $(this).text().toLowerCase();
+        if(input == a){                 
+           var temp = $(this).parent().find($('td:nth-child(3)')).text();
+           
+          $('label','.items').each(function(i) {
+            var b = $(this).text();
+            
+            if(temp == b){
+             var alm = $(this).attr('for');
+             var fini = $(this).parent().find($("input[id='"+alm+"']"));
+             fini.prop('checked', true);
+            }
+          });
+        }            
+      });
+
+    //Bonus:
+    $("dfn.ol").parent().each(function(){
+      var oristr = $(this).html();
+      var mySubString = oristr.substring(
+        oristr.lastIndexOf('ol">') + 4,
+        oristr.indexOf("/dfn>") - 1
+      );
+      //console.log(mySubString);
+      if(input == mySubString){
+        //console.log(input);
+      var temp = $(this).parent().html();
+      //console.log(temp);
+      var temp1 = temp.replace('<b>',"");
+      var temp2 = temp1.replace('</b>',"");
+      var temp3 = temp2.replace(mySubString,"");
+      var temp4 = temp3.replace('<dfn class="ol"></dfn>',"");
+      //console.log(temp4);
+          $('label','.items').each(function(i) {
+            var b = $(this).html();
+            
+            if(temp4 == b){
+             var alm = $(this).attr('for');
+             var fini = $(this).parent().find($("input[id='"+alm+"']"));
+             fini.prop('checked', true);
+            }
+          });
+      }     
+    });  
+    //END Bonus
+
+
+    if (answers.hasOwnProperty(input) && !answers[input]) {
+      answers[input] = true;
+   var msg = new SpeechSynthesisUtterance(input);
+
+   
+
+   if (voiceSelect.value) {
+    msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+   }
+   window.speechSynthesis.speak(msg);
+      score++;
+      $('.score').text(score);
+      $('.scored-answers').prepend(createAnswerItem(input));
+      $('.input').val('');
+      if (score === QUIZ_ANSWERS.length) {
+        endQuiz();
+      }
+    }
+  }
+
+  function endQuiz() {
+    // freeze
+    clearTimeout(timeInterval);
+    $('.input').prop('disabled', true);
+    
+    // calculate percentage
+    var percent = Math.round(score / QUIZ_ANSWERS.length * 100);
+    $('.percent').text(percent);
+
+    // change status stuff
+    $('.status-timer, .status-current-score').hide();
+    $('.status-final-results').show();
+    $('.footnote').show();
+
+    // score-dependent stuff
+    if (score === QUIZ_ANSWERS.length) {
+      // happy
+      $('.fa-chevron-circle-up').css('display','block');
+      $('.fa-chevron-circle-up').animate({bottom:'90px'});
+
+      $('.end-greeting').text('Perfect, with ' + getTimeString() + ' remaining!');
+      setToastMood('happy', true);
+      var msg = new SpeechSynthesisUtterance('Congratulations');
+       if (voiceSelect.value) {
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+       }
+   window.speechSynthesis.speak(msg);
+    } else if (score > 0) {
+      // neutral
+      $('.end-greeting').text('Time\'s up!');
+      setToastMood('neutral', true);
+      renderMissedAnswers();
+      $('.status-toggle-answers').show();
+    } else {
+      // sad
+      $('.end-greeting').text('Nothing...');
+      setToastMood('sad', true);
+      renderMissedAnswers();
+      $('.scored-answers').hide();
+      $('.missed-answers').show();
+    }
+  }
+
+  function renderMissedAnswers() {
+    QUIZ_ANSWERS.forEach(function(item) {
+      var answer = item.trim().toLowerCase()
+      if (!answers[answer]) {
+        $('.missed-answers').append(createAnswerItem(answer));
+      }
+    });
+  }
+
+  function toggleAnswers(event) {
+    event.preventDefault();
+    
+    if ($('.scored-answers').is(':visible')) {
+      // switch to missed answers
+      $('.toggle').text('see what you got');
+      $('.scored-answers').hide();
+      $('.missed-answers').show();
+    } else {
+      // switch to scored answers
+      $('.toggle').text('see what you missed');
+      $('.missed-answers').hide();
+      $('.scored-answers').show();
+    }
+  }
+
+  function createAnswerItem(answer) {
+    return $('<li>', { text: answer });
+  }
+
+  function getTimeString() {
+    if (timeRemaining <= 0) {
+      return '0:00';
+    } else {
+      var minutes = Math.floor(timeRemaining / 60);
+      var seconds = timeRemaining % 60;
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+      return minutes + ':' + seconds;
+    }
+  }
+
+  function reset() {
+    // put everything back the way it was
+    $('.started, .status-final-results, .status-toggle-answers, .missed-answers, .footnote').hide();
+    $('.start, .status-timer, .status-current-score, .scored-answers').show();
+    $('.input').prop('disabled', false).val('');
+    $('.answers').empty();
+    $('.toggle').text('see what you missed');
+    setToastMood('neutral', true);
+    $('.items input').prop('checked', false);
+    return focusr = true;
+  }
+
+
+  const wrapper = document.querySelector(".input-wrapper"),
+        textInput = document.querySelector("input[type='text']");
+          
+  textInput.addEventListener("keyup", event => {
+    wrapper.setAttribute("data-text", event.target.value);
+  });
+
+  $(function () {
+    // fill in the blanks
+    
+    // bind events
+    $('.start').on('click', function(){
+      startQuiz();
+      
+    });
+    $('.input').on('input', checkInput);
+    $('.toggle').on('click', toggleAnswers);
+    $('.reset').on('click', function(){
+     $('.items').find('*').not('h2').remove();
+     sulist = shuffle(sulist);
+     for(var id1 = 0; id1 < sulist.length; id1 ++){
+   $('.items').prepend('<input id="item'+ id1 +'" type="checkbox" onclick="return false" onkeydown="return false"><label for="item' + id1 +'">' + sulist[id1] + '</label>');
+  }
+     reset();
+
+    });
+
+  });
+
+$(document).on("keypress", "form", function(event) { 
+    return event.keyCode != 13;
+});
+
+$('#m tbody tr td:nth-child(1)').each(function(){
+  $(this).append('<img src="../photos\\speaker.png"/>');
+})
+$('#m tbody tr td:nth-child(1) img').each(function(){ 
+  $(this).click(function(){
+     var z = $(this).parent().text();
+     var msg = new SpeechSynthesisUtterance(z);
+    if (voiceSelect.value) {
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+       }
+       window.speechSynthesis.speak(msg);
+  });  
+});
+  document.addEventListener("DOMContentLoaded",function(){
+    var butt = document.querySelector('.butt'),      //Only the first element of this class is selected
+        wrap_menu = document.querySelector('.wrap_menu'),
+        overlay = document.querySelector('.overlay'),
+        layer_butt = document.querySelector('.layer2'),
+        menu = document.getElementsByClassName('list-group-item');
+
+    butt.onclick = function(){
+      overlay.classList.add('show');
+      layer_butt.classList.add('rotate3D');
+      wrap_menu.classList.remove('moveleft'); 
+      $('#relax').css('transform','translateY(100%)');
+      $('.fa-chevron-circle-up').css('transform','translateY(140px)');
+    }
+    
+    overlay.onclick = function(){
+      overlay.classList.remove('show');
+      layer_butt.classList.remove('rotate3D');
+      wrap_menu.classList.add('moveleft');
+      $('#relax, .fa-chevron-circle-up').css('transform','translateY(0)');
+    }
+  },false);
+
+$('.layer2').bind('heightChange', function(){
+  if(!$('.layer2').hasClass("rotate3D")){                                               
+    var height1 = $('.layer2').height();
+    $('.overlay,.layer4').css('height',height1);
+  }else if($('.layer2').hasClass('rotate3D') == true){
+    var height2 = $('.layer2').height();
+    $('.overlay,.layer4').css('height',height2);
+  }
+});
+$(".butt").click(function() {
+    $(".layer2").trigger('heightChange'); 
+});    
+function topFunction() {
+    //document.body.scrollTop = 0;
+    //document.documentElement.scrollTop = 0;
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+}
+
+$(".dropdown-btn").on('click',function(){
+  var a = this.id;
+  (this.firstClk = !this.firstClk) ? a1(): a2();
+   function a1(){
+    $('.dropdown-btn#' + a + ' i.fa-caret-right').replaceWith( '<i class="fa fa-caret-down"></i>' );
+    $('.dropdown-btn#' + a).addClass('visible');
+    $('.dropdown-btn#' + a).on('animationend webkitAnimationEnd oAnimationEnd', function () {
+        $('.dropdown-btn#' + a +'.visible .dropdown-container li').each(function(index){
+            $(this).css({
+              'animation-delay':index*0.1+0.7+'s',
+              'visibility':'visible'
+            });
+        });
+    });
+
+ 
+    
+   }
+   function a2(){
+    $('.dropdown-btn#' + a + ' i.fa-caret-down').replaceWith( '<i class="fa fa-caret-right"></i>' );
+
+    $('.dropdown-btn#' + a).removeClass('visible');
+   }
+});
+
+$('.butt,.display a').addClass('rainbow');
+function getRand(bottom, top) {  
+  return Math.floor( Math.random() * ( 1 + top - bottom ) ) + bottom;
+}
+var result = getRand(1,15);
+var iDiv = document.createElement('div');
+iDiv.id = 'relax';
+document.getElementsByTagName('body')[0].appendChild(iDiv);
+var sound      = document.createElement('audio');
+document.getElementById('relax').appendChild(sound);
+sound.id       = 'audio-player';
+sound.controls = 'controls';
+sound.src      = '../music/' + result + '.mp3';
+sound.loop = true;
+sound.load();
+$('li u').parent().css('line-height','25px');
+$('#relax').append('<div id="pli"><span>Nhạc nền:</span><select id="sel1"></select></div><div class="love"><p>Made with <img src="../photos/love.png" /> by <a href="https://www.facebook.com/nmquang.hust" target="_blank"> Minh Quang </a></p></div>');
+
+for(var i = 1; i<=15; i++){
+    $('#relax select').append('<option value="' + i +'">' + i + '.mp3</option>')
+}
+$('#relax select').val(result);
+$('#relax select').on('change', function (e) {
+    var optionSelected = $("#relax option:selected", this);
+    var valueSelected = this.value;
+    $('#relax audio').attr('src','../music/' + valueSelected + '.mp3');
+});
+$('#pli,.love').css('top',$('#relax').height()/2);
+function boldString(str, find){
+    var re = new RegExp(find, 'g');
+    return str.replace(re, '<b>'+find+'</b>');
+}
+$('tr td:nth-child(4)', '#m').each(function(i) {
+  if($(this).parent().find($('td:nth-child(1)')).text() != 'pubes'){
+      var b = $(this).html();
+      var cc = [];
+      $('div.note', '#m').each(function(){
+         var c = $(this).html();
+         cc.push(c);
+      });
+
+      for(var i = 0; i < cc.length; i ++){
+        b = b.replace(cc[i],'');
+      }
+      var a = $(this).parent().find($('tr td:nth-child(1)')).text();
+      var result = boldString(b, a);  
+      $(this).html(result);
+      var divs = $('div.note');
+
+for(var i = 0; i < divs.length; i++){
+    $(divs[i]).html(cc[i]); 
+}
+      
+  }  
+});
+
+var flag = true;
+  $('dfn').mouseover(function(){
+    var a = $(this).text();
+    timer = setTimeout(function(){
+      if(flag){
+        var msg = new SpeechSynthesisUtterance(a);
+      if (voiceSelect.value) {
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+       }     
+        window.speechSynthesis.speak(msg); 
+      }    
+      flag = false;     
+    },1000);  
+  }).mouseleave(function() {
+    clearTimeout(timer);
+    flag = true;
+  });
+
+$('div.note li').addClass('exc');
+$('#m tbody tr td:nth-child(4) li:not(.exc)').each(function(){
+  $(this).prepend('<i class="fa fa-volume-up" style="margin-right:15px;cursor:pointer;"></i>');
+});
+$('#m tbody tr td:nth-child(4) li:not(.exc) i').each(function(){ 
+  $(this).click(function(){
+     var z = $(this).parent().text();
+     var msg = new SpeechSynthesisUtterance(z);
+    if (voiceSelect.value) {
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+       }
+       window.speechSynthesis.speak(msg);
+  });  
+});
+
+
